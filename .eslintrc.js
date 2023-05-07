476a02cc8752a3c50b10ff4e4de54b9b647b22fb
Nikolay <nickyanfe@yandex.ru>
2023-05-07 19:18:42 +0300

    13 project 1

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,14 +3,14 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ["plugin:react/recommended", "airbnb-base"],
+  extends: ['plugin:react/recommended', 'airbnb-base'],
   overrides: [],
   parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
-  plugins: ["react"],
+  plugins: ['react'],
   rules: {
-    "no-underscore-dangle": ["error", { allow: ["_id"] }],
+    'no-underscore-dangle': ['error', { allow: ['_id'] }],
   },
 };
