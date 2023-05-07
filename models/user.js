e7881e3298cd3f7275b075abf6f6856cf5b09827
Nikolay <nickyanfe@yandex.ru>
2023-05-07 19:16:16 +0300

    13 project 1

--- models/user.js
+++ models/user.js
@@ -1,31 +1,31 @@
-const mongoose = require("mongoose");
-const validator = require("validator");
+const mongoose = require('mongoose');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema(
   {
     name: {
       type: String,
-      required: [true, "Это поле обязательно к заполнению"],
-      minlength: [2, "Минимальная длина текста в данном поле = 2 знака "],
-      maxlength: [30, "Максимальная длина текста в данном поле = 30 знаков "],
+      required: [true, 'Это поле обязательно к заполнению'],
+      minlength: [2, 'Минимальная длина текста в данном поле = 2 знака '],
+      maxlength: [30, 'Максимальная длина текста в данном поле = 30 знаков '],
     },
     about: {
       type: String,
-      required: [true, "Это поле обязательно к заполнению"],
-      minlength: [2, "Минимальная длина текста в данном поле = 2 знака "],
-      maxlength: [30, "Максимальная длина текста в данном поле = 30 знаков "],
+      required: [true, 'Это поле обязательно к заполнению'],
+      minlength: [2, 'Минимальная длина текста в данном поле = 2 знака '],
+      maxlength: [30, 'Максимальная длина текста в данном поле = 30 знаков '],
     },
     avatar: {
       type: String,
       validate: {
         validator: (v) => validator.isURL(v),
-        message: "Некорректный URL",
+        message: 'Некорректный URL',
       },
-      required: [true, "Это поле обязательно к заполнению"],
+      required: [true, 'Это поле обязательно к заполнению'],
     },
   },
 
-  { versionKey: false }
+  { versionKey: false },
 );
 
-module.exports = mongoose.model("user", userSchema);
+module.exports = mongoose.model('user', userSchema);
--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,4 @@
-const userRoutes = require("express").Router();
+const userRoutes = require('express').Router();
 
 const {
   getUsers,
@@ -6,12 +6,12 @@ const {
   createUser,
   updateUser,
   updateAvatar,
-} = require("../controllers/users");
+} = require('../controllers/users');
 
-userRoutes.get("/", getUsers);
-userRoutes.get("/:userId", getUserById);
-userRoutes.post("/", createUser);
-userRoutes.patch("/me", updateUser);
-userRoutes.patch("/me/avatar", updateAvatar);
+userRoutes.get('/', getUsers);
+userRoutes.get('/:userId', getUserById);
+userRoutes.post('/', createUser);
+userRoutes.patch('/me', updateUser);
+userRoutes.patch('/me/avatar', updateAvatar);
 
 module.exports = userRoutes;
