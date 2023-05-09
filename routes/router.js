287d1ac10623d68ac57563c351bed012bcf153ca
Nikolay <nickyanfe@yandex.ru>
2023-05-09 18:24:07 +0300

    13 project 3

--- routes/router.js
+++ routes/router.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { NOT_FOUND } = require('../utils/errors');
 
 const usersRouter = require('./users');
 const cardsRouter = require('./cards');
@@ -6,7 +7,7 @@ const cardsRouter = require('./cards');
 router.use('/users', usersRouter);
 router.use('/cards', cardsRouter);
 router.use('/*', (req, res) => {
-  res.status(404).send({ message: '404: Ошибка!' });
+  res.status(NOT_FOUND).send({ message: '404: Ошибка!' });
 });
 
 module.exports = router;
